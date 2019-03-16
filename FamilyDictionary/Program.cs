d605c75200ce6854c067f2a1682217b15909f2d0
Jasmine Walters <jsmnwltrs@gmail.com>
2019-03-16 10:34:01 -0500

    created my family

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,18 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("mistress", new Dictionary<string, string>() { { "name", "Tonya Roxanne" }, { "age", "28" }});
+            myFamily.Add("doggo", new Dictionary<string, string>() { { "name", "Doofus" }, { "age", "4" } });
+
+            foreach (var (member,info) in myFamily)
+            { 
+              Console.WriteLine($"{myFamily[member]["name"]} is my {member} and is {myFamily[member]["age"]} years old");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
